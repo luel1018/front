8776954c78163ef42199368768fb1fddc85cd451
luel1018 <luper1018@yonsei.ac.kr>
2026-02-24 16:58:18 +0900

    [추가] 회원가입

---
+++ front/src/views/user/Signup.vue
@@ -0,0 +1,26 @@
+<script setup>
+import api from '@/api/user'
+import { reactive } from 'vue'
+
+const userInput = reactive({
+  email: '',
+  name: '',
+  password: '',
+})
+
+const submitForm = async () => {
+  const data = await api.signup(userInput)
+}
+</script>
+
+<template>
+  <form @submit.prevent="submitForm">
+    <p>이메일 <input name="email" v-model="userInput.email" /></p>
+    <p>이름 <input name="name" v-model="userInput.name" /></p>
+    <p>비밀번호 <input type="password" name="password" v-model="userInput.password" /></p>
+
+    <button>회원 가입</button>
+  </form>
+</template>
+
+<style scoped></style>
