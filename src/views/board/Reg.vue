7d984dba246bba2c9e579977e7263b3b52a63975
luel1018 <luper1018@yonsei.ac.kr>
2026-02-24 12:19:00 +0900

    init: front project setup

---
+++ src/views/board/Reg.vue
@@ -0,0 +1,28 @@
+<script setup>
+import api from '@/api/board'
+import { reactive } from 'vue'
+
+const boardInput = reactive({
+  title: '',
+  contents: '',
+})
+
+const submitForm = async () => {
+  const data = await api.reg(boardInput)
+}
+</script>
+
+<template>
+  <form @submit.prevent="submitForm">
+    <p>
+      <input name="title" v-model="boardInput.title" />
+    </p>
+    <p>
+      <textarea name="contents" v-model="boardInput.contents" />
+    </p>
+
+    <button>게시글 작성</button>
+  </form>
+</template>
+
+<style scoped></style>
