942670bb2d57f78186a8ab32cb3760775f106c7e
luel1018 <luper1018@yonsei.ac.kr>
2026-02-24 17:14:40 +0900

    [추가] 게시글 리스트 보여주기

---
+++ front/src/views/board/List.vue
@@ -0,0 +1,23 @@
+<script setup>
+import api from '@/api/board'
+import { onMounted, reactive } from 'vue'
+
+const boardList = reactive([])
+
+const getBoardList = async () => {
+  const res = await api.list()
+  boardList.push(...res)
+}
+onMounted(() => {
+  getBoardList()
+})
+</script>
+
+<template>
+  <p v-for="post in boardList">
+    <span>{{ post.idx }}</span
+    ><span>{{ post.title }}</span>
+  </p>
+</template>
+
+<style scoped></style>
