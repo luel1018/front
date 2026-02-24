69cbea025b9fdd50860167c76cdea9d0120ac44d
luel1018 <luper1018@yonsei.ac.kr>
2026-02-24 16:53:27 +0900

    init: 헤더컴포넌트 + 게시글 기능들

---
+++ front/src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div>
+    <RouterLink :to="{ name: 'boardList' }">목록</RouterLink>
+    <RouterLink :to="{ name: 'boardReg' }">작성</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+a {
+  margin-right: 1rem;
+}
+</style>
